d876d1ee9070538dacde032c5229c2c33d656630
vanitahvz-hub <vanitahvz@gmail.com>
2026-09-15 04:53:32 +0400

    Update _lib.js

--- api/_lib.js
+++ api/_lib.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const API_VERSION = "2024-01";
+const API_VERSION = "2026-07";
 
 export function db() {
   const url = process.env.SUPABASE_URL;
